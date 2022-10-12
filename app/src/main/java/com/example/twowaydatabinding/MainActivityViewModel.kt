d40f0eae92a4684fc6ca17ea0ecee0204a65e422
seunggyu <seunggyu97@gmail.com>
2022-10-12 23:56:00 +0900

    First Commit

---
+++ app/src/main/java/com/example/twowaydatabinding/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.twowaydatabinding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    val userName = MutableLiveData<String>()
+
+    init{
+        userName.value = "Seunggyu"
+    }
+}
